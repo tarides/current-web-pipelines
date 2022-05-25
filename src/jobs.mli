ec174c117d48ef9cb0bb4f26c90c6ee9972a1673
Navin Keswani <navin@novemberkilo.com>
2022-05-25 13:47:44 +1000

    Change ocaml version lower bound.

--- src/jobs.mli
+++ src/jobs.mli
@@ -1,14 +1,16 @@
 (*
 This module contains utilities for manipulating job_trees
 *)
-val job_tree_to_job_ids : ('output, 'node_metadata) State.job_tree -> string list
-(* Given a job_tree, extract all the job_ids of the jobs in the tree into a list*)
+val job_tree_to_job_ids :
+  ('output, 'node_metadata) State.job_tree -> string list
+(** Given a job_tree, extract all the job_ids of the jobs in the tree into a list*)
 
 val failed : 'output State.job_result -> bool
-(* encode what job results correspond to failures and what do not*)
+(** encode what job results correspond to failures and what do not*)
 
 val rebuildable_jobs :
-  node_map_status:('node_metadata -> 'output State.job_result -> 'output State.job_result) ->
+  node_map_status:
+    ('node_metadata -> 'output State.job_result -> 'output State.job_result) ->
   ('output, 'node_metadata, _) State.stage list ->
   string list
-(* Given a list of stages, extract the job_ids of rebuildable jobs (i.e. jobs that have failed)*)
+(** Given a list of stages, extract the job_ids of rebuildable jobs (i.e. jobs that have failed)*)
